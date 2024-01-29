585c43266fe8abb86c6ed42bb645024786a97cc2
Dharvik19 <dharvik.badga22@gmail.com>
2024-01-29 11:31:21 +0530

    Updating index js for redux store

--- pets/src/Store/index.js
+++ pets/src/Store/index.js
@@ -1,8 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit'
-
+import DropDownReducer from './DropDownSlice';
+import PetFormReducer from './PetFormSlice';
 const store = configureStore({
     reducer : {
-        
+        dropdown : DropDownReducer,
+        petform : PetFormReducer,
     }
 })
 
